ccd803eb5eb648ed14d164a67f31743893b443c3
Jon Ruskin <jon.ruskin@gmail.com>
2019-09-15 17:20:26 -0700

    update code and packages

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const { exec } = require('@actions/exec');
+const fs = require('fs').promises;
+const io = require('@actions/io');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const token = core.getInput('github_token', { required: true });
+    const commitMessage = core.getInput('commit_message', { required: true });
+    const userName = core.getInput('user_name', { required: true });
+    const userEmail = core.getInput('user_email', { required: true });
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const command = core.getInput('command', { required: true });
+    await io.which(command.split(' ')[0], true); // check that command is runnable
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
+    const configFilePath = core.getInput('config_file', { required: true });
+    await fs.access(configFilePath); // check that config file exists
+
+    let branch = process.env.GITHUB_REF;
+    if (!branch.startsWith('refs/heads')) {
+      throw new Error(`${branch} does not reference a branch`);
+    }
+    branch = branch.replace('refs/heads/', '');
+
+    await exec('git', ['checkout', branch]);
+    await exec(command, ['cache', '-c', configFilePath]);
+    await exec('git', ['add', '.']);
+
+    const exitCode = await exec('git', ['diff-index', '--quiet', 'HEAD'], { ignoreReturnCode: true });
+    if (exitCode > 0) {
+      await exec('git', ['remote', 'add', 'licensed-ci-origin', `https://x-access-token:${token}@github.com/${process.env.GITHUB_REPOSITORY}.git`]);
+      await exec('git', ['-c', `user.name=${userName}`, '-c', `user.email=${userEmail}`, 'commit', '-m', commitMessage]);
+      await exec('git', ['push', 'licensed-ci-origin', branch]);
+    }
+
+    await exec(command, ['status', '-c', configFilePath]);
+  }
   catch (error) {
     core.setFailed(error.message);
   }
