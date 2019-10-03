991b170e8d53d8b65799061bc1060b0a1f1e6e67
Jon Ruskin <jon.ruskin@gmail.com>
2019-10-02 23:36:03 -0700

    allow counts on mocks

--- test/mocks/@actions/exec.js
+++ test/mocks/@actions/exec.js
@@ -39,6 +39,14 @@ sinon.stub(exec, 'exec').callsFake(async (command, args = [], options = {}) => {
     if (mock.exitCode || mock.exitCode === 0) {
       exitCode = mock.exitCode;
     }
+
+    if (mock.count > 0) {
+      mock.count -= 1;
+      if (mock.count === 0) {
+        const index = mocks.indexOf(mock);
+        mocks.splice(index, 1);
+      }
+    }
   }
 
   if (exitCode !== 0 && !options.ignoreReturnCode) {
